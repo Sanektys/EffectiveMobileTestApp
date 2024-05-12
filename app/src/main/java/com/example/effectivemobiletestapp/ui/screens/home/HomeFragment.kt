9bf6d241be942e9e6022345cc646335f52b61fff
Ilya Shesterov <Sanektisru@gmail.com>
2024-05-12 23:03:35 +0700

    Created and added search screen via modal bottom sheet

--- app/src/main/java/com/example/effectivemobiletestapp/ui/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/effectivemobiletestapp/ui/screens/home/HomeFragment.kt
@@ -3,10 +3,13 @@ package com.example.effectivemobiletestapp.ui.screens.home
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.effectivemobiletestapp.R
 import com.example.effectivemobiletestapp.databinding.FragmentHomeBinding
+import com.example.effectivemobiletestapp.ui.screens.search.SearchModalBottomSheet
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
@@ -19,10 +22,14 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentHomeBinding.bind(view)
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        val toCountryTextView = binding.countryTo
+        toCountryTextView.setOnClickListener {
+            SearchModalBottomSheet().let { dialog ->
+                dialog.setStyle(DialogFragment.STYLE_NORMAL, R.style.BottomSheetDialogTheme)
+                dialog.show(childFragmentManager, SearchModalBottomSheet.TAG)
+            }
+
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/effectivemobiletestapp/ui/screens/search/SearchModalBottomSheet.kt
+++ app/src/main/java/com/example/effectivemobiletestapp/ui/screens/search/SearchModalBottomSheet.kt
@@ -1,12 +1,21 @@
 package com.example.effectivemobiletestapp.ui.screens.search
 
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.example.effectivemobiletestapp.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class SearchModalBottomSheet : BottomSheetDialogFragment(R.layout.fragment_search_modal_bottom_sheet) {
 
     private lateinit var viewModel: SearchViewModel
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        (view.parent as View).layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+        (dialog as BottomSheetDialog).behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
 
     companion object {
         const val TAG = "SearchModalBottomSheet"
