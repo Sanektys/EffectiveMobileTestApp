e90ec7ea107dae975b73373aa162615eaa295f87
Ilya Shesterov <Sanektisru@gmail.com>
2024-05-15 01:45:19 +0700

    All placeholder screens brought to the same view

--- app/src/main/java/com/example/effectivemobiletestapp/ui/screens/hotels/HotelsFragment.kt
+++ app/src/main/java/com/example/effectivemobiletestapp/ui/screens/hotels/HotelsFragment.kt
@@ -1,32 +1,6 @@
 package com.example.effectivemobiletestapp.ui.screens.hotels
 
-import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.effectivemobiletestapp.R
-import com.example.effectivemobiletestapp.databinding.FragmentHotelsBinding
 
-class HotelsFragment : Fragment(R.layout.fragment_hotels) {
-
-    private var _binding: FragmentHotelsBinding? = null
-    private val binding get() = _binding!!
-
-    private val homeViewModel by lazy { ViewModelProvider(this).get(HotelsViewModel::class.java) }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        _binding = FragmentHotelsBinding.bind(view)
-
-        val textView: TextView = binding.textDashboard
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+class HotelsFragment : Fragment(R.layout.fragment_hotels)
--- app/src/main/java/com/example/effectivemobiletestapp/ui/screens/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/effectivemobiletestapp/ui/screens/profile/ProfileFragment.kt
@@ -1,32 +1,6 @@
 package com.example.effectivemobiletestapp.ui.screens.profile
 
-import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.effectivemobiletestapp.R
-import com.example.effectivemobiletestapp.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
-
-    private var _binding: FragmentProfileBinding? = null
-    private val binding get() = _binding!!
-
-    private val homeViewModel by lazy { ViewModelProvider(this).get(ProfileViewModel::class.java) }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        _binding = FragmentProfileBinding.bind(view)
-
-//        val textView: TextView = binding.textDashboard
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+class ProfileFragment : Fragment(R.layout.fragment_profile)
